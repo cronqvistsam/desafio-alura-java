da382ec3701fc6e21698f7d0018f36af2a4559ee
Sam <samara.silva03k@gmail.com>
2024-07-09 02:25:57 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ while (contador <= 10) {
 //ao contrário 
 // >= 0
 //contador--
+
+
+//desafio contagem regressiva 
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+
+//ao contrário alterar 
+//let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+///let contador = 0;
+
+//while (contador <= numeroMaximo) {
+//    console.log(contador);
+//    contador++
+//}
